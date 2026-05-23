2c121f3fef8557a464d6049777c4b3d44f640de6
Ahmed elbadri <aelbadri23@gmail.com>
2026-05-23 21:24:53 +0300

    feat: add NoteModel Hive entity and initialize Hive storage in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive_project/views/view_note.dart';
+import 'package:hive_project/models/note_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyApp());
 }
 
--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,17 @@
-class NoteModel {
-  final String title;
-  final String subTitle;
-  final String data;
-  final int color;
+import 'package:hive_flutter/adapters.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  String title;
+  @HiveField(1)
+  String subTitle;
+  @HiveField(2)
+  String data;
+  @HiveField(3)
+  int color;
   NoteModel({
     required this.title,
     required this.subTitle,
