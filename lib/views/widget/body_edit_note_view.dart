20d188a0bd0bb204657ac29df87b6dd5a3d49659
Ahmed elbadri <aelbadri23@gmail.com>
2026-05-23 19:26:03 +0300

    feat: create BodyEdidNoteView with title and content text fields

--- lib/views/widget/body_edit_note_view.dart
+++ lib/views/widget/body_edit_note_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:hive_project/views/widget/cutom_text_feild.dart'
+    show CutsomTextFeild;
 
-class BodyEdidNoteView extends StatelessWidget {
+class BodyEdidNoteView extends StatefulWidget {
   const BodyEdidNoteView({super.key});
 
   @override
+  State<BodyEdidNoteView> createState() => _BodyEdidNoteViewState();
+}
+
+class _BodyEdidNoteViewState extends State<BodyEdidNoteView> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController contentController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        children: [
+          CutsomTextFeild(hintText: 'title', controller: titleController),
+          SizedBox(height: 16),
+          CutsomTextFeild(
+            hintText: 'content',
+            controller: contentController,
+            maxLines: 5,
+          ),
+        ],
+      ),
+    );
   }
 }
